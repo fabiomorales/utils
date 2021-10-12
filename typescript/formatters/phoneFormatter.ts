6f6f8f14ba4e0b02c4fb8f78b4b30da4c3c2b758
Fabio Morales <fabio.l.morales@hotmail.com>
2021-10-12 12:41:03 -0300

    feat(PhoneFormatter): Create PhoneFormatter Function Typescript

---
+++ typescript/formatters/phoneFormatter.ts
@@ -0,0 +1,9 @@
+const phoneFormatter = (phone: string) => {
+  const firstElement = phone.slice(3, 5);
+  const secondElement = phone.slice(5, 10);
+  const thirdElement = phone.slice(10, 14);
+
+  return `(${firstElement}) ${secondElement}-${thirdElement}`;
+};
+
+export default phoneFormatter;
